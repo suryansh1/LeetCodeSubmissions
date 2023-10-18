865bae8bfd16f5f4cf7f07138bc7bd443ff80480
Suryansh D Kumar <email.suryansh@gmail.com>
2023-10-18 00:02:56 -0400

    Time: 104 ms (19.18%), Space: 17.9 MB (38.93%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -2,38 +2,38 @@ class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         
         ####### EXTRA SPACE #####
-        # result = []
-        # for i, val in enumerate(nums):
+        result = []
+        for i, val in enumerate(nums):
 
-        #     if result == [] :
-        #         result.append(val)
-        #         continue
+            if result == [] :
+                result.append(val)
+                continue
 
-        #     if val > result[-1] : 
-        #         result.append(val)
+            if val > result[-1] : 
+                result.append(val)
 
-        # for i, val in enumerate(result):
-        #     nums[i] = val
+        for i, val in enumerate(result):
+            nums[i] = val
 
-        # return len(result)
+        return len(result)
 
         ##### TWO POINTERS #####
 
-        ptr1, ptr2 = 0, 1
+#         ptr1, ptr2 = 0, 1
 
-        size = len(nums)
+#         size = len(nums)
         
-        while(ptr2 < size):
+#         while(ptr2 < size):
 
-            while nums[ptr2] == nums[ptr1]:
-                ptr2 +=1
+#             while nums[ptr2] == nums[ptr1]:
+#                 ptr2 +=1
                 
-                if ptr2 >= size : return ptr1 + 1
+#                 if ptr2 >= size : return ptr1 + 1
 
-            ptr1 += 1
+#             ptr1 += 1
 
-            nums[ptr1] = nums[ptr2]
+#             nums[ptr1] = nums[ptr2]
 
-            ptr2 += 1
+#             ptr2 += 1
 
-        return ptr1 + 1
+#         return ptr1 + 1
