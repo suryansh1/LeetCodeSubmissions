18a4c9a9f39a764b26dc3f759f1193ab6cf2470a
Suryansh D Kumar <email.suryansh@gmail.com>
2023-12-17 20:57:37 -0500

    Time: 40 ms (67.89%), Space: 16.3 MB (16.26%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.py
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.py
@@ -1,37 +1,49 @@
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
         
-        x = 101
         
-        operations = 0
+        mySet = set()
         
-        while (1):
+        for num in nums :
+            if num >0 and num not in mySet :
+                mySet.add(num)
+                
+        return len(mySet)
+        
+        
+        
+############## Slow method by Performing Operations #############        
+#         x = 101
+        
+#         operations = 0
+        
+#         while (1):
             
-            x = 101
+#             x = 101
             
-            # Finding min and assigning to x
-            for num in nums :
+#             # Finding min and assigning to x
+#             for num in nums :
 
-                if num > 0 :
-                    if num < x : 
-                        x = num 
+#                 if num > 0 :
+#                     if num < x : 
+#                         x = num 
                         
-            if x == 101 : return operations
+#             if x == 101 : return operations
                   
             
-            flag = 0
+#             flag = 0
             
-            # Subtracting x from each element                    
-            for i, num in enumerate(nums):
+#             # Subtracting x from each element                    
+#             for i, num in enumerate(nums):
 
-                if num > 0:
-                    nums[i]-=x
+#                 if num > 0:
+#                     nums[i]-=x
                     
-                    if nums[i]!=0 : 
-                        flag = 1
+#                     if nums[i]!=0 : 
+#                         flag = 1
                         
-            operations +=1
+#             operations +=1
             
-            if not flag : return operations
+#             if not flag : return operations
                             
             
