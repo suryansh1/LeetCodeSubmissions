e71455e438edb278c39719abbeeefddc640b9550
Suryansh D Kumar <email.suryansh@gmail.com>
2023-12-17 20:53:09 -0500

    Time: 48 ms (25.98%), Space: 16.2 MB (82.29%) - LeetHub

---
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.py
@@ -0,0 +1,37 @@
+class Solution:
+    def minimumOperations(self, nums: List[int]) -> int:
+        
+        x = 101
+        
+        operations = 0
+        
+        while (1):
+            
+            x = 101
+            
+            # Finding min and assigning to x
+            for num in nums :
+
+                if num > 0 :
+                    if num < x : 
+                        x = num 
+                        
+            if x == 101 : return operations
+                  
+            
+            flag = 0
+            
+            # Subtracting x from each element                    
+            for i, num in enumerate(nums):
+
+                if num > 0:
+                    nums[i]-=x
+                    
+                    if nums[i]!=0 : 
+                        flag = 1
+                        
+            operations +=1
+            
+            if not flag : return operations
+                            
+            
