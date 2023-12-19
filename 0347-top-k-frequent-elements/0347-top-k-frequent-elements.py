5fdc2c32c64173b7104c02783e05b15675a93973
Suryansh D Kumar <email.suryansh@gmail.com>
2023-12-19 00:05:05 -0500

    Time: 92 ms (86.43%), Space: 21 MB (79.06%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,6 +1,9 @@
+from collections import Counter
+
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        
+    
+    def topKFrequentDictDelete(self, nums: List[int], k: int) -> List[int]:
+    
         myDict = {}
         
         for num in nums:
@@ -20,6 +23,53 @@ class Solution:
             
             ret.append(key)
             
+            # Deletion in dict is O(n), as elements need to be shifted          
             del(myDict[key])
             
         return ret
+        
+    def topKFrequentDictSort(self, nums: List[int], k: int) -> List[int]:
+        
+        myDict = {}
+        
+        for num in nums:
+            
+            if num not in myDict:
+                myDict[num] = 1
+                
+            else :
+                myDict[num] += 1
+                
+        return sorted(myDict, key=myDict.get, reverse=True)[:k]
+
+    def topKFrequentCounter(self, nums: List[int], k: int) -> List[int]:
+        
+        counter = Counter(nums)
+        
+        return [key for key, val in counter.most_common(k)]
+
+    def topKFrequentHeap(self, nums: List[int], k: int) -> List[int]:
+        
+        import heapq
+
+        myDict = {}
+        
+        for num in nums:
+            
+            if num not in myDict:
+                myDict[num] = 1
+                
+            else :
+                myDict[num] += 1
+                
+        return heapq.nlargest(k, myDict, key=myDict.get)
+
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        
+        # return self.topKFrequentDictDelete(nums, k)
+        
+        # return self.topKFrequentDictSort(nums, k)
+        
+        # return self.topKFrequentCounter(nums, k)
+    
+        return self.topKFrequentHeap(nums, k)
