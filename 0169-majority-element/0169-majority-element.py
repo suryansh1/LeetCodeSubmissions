f342ae2acbace05e4dfe10f73810cb9c27198bea
Suryansh D Kumar <email.suryansh@gmail.com>
2023-12-24 09:42:31 -0500

    Time: 140 ms (92.29%), Space: 18.7 MB (5.04%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,8 +1,37 @@
 from collections import Counter
 
 class Solution:
-    def majorityElement(self, nums: List[int]) -> int:
+
+    def majorityElementHashMap(self, nums: List[int]) -> int:
         
         counter = Counter(nums)
 
         return counter.most_common(1)[0][0]
+
+
+    def majorityElementBeyerMoore(self, nums: List[int]) -> int:
+
+        candidate = nums[0]
+
+        count = 0
+
+        for num in nums :
+
+            if candidate == num :
+                count +=1
+            
+            else :
+                count -= 1
+
+                if not count : 
+                    candidate = num
+
+                    count = 1
+
+        return candidate
+
+    def majorityElement(self, nums: List[int]) -> int:
+        
+        # return self.majorityElementHashMap(nums)
+
+        return self.majorityElementBeyerMoore(nums)
