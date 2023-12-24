abcc1835d7ac18eaa11ad95073f8f53d6e8e2f0d
Suryansh D Kumar <email.suryansh@gmail.com>
2023-12-24 09:22:26 -0500

    Time: 39 ms (65.28%), Space: 17.3 MB (8.86%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.py
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -1,8 +1,8 @@
 from collections import Counter
 
 class Solution:
-    def longestPalindrome(self, s: str) -> int:
-        
+
+    def longestPalindromeDict(self, s: str) -> int:
         counter = Counter(s)
 
         # Construct palindrome with largest odd letters in the middle and 
@@ -13,7 +13,6 @@ class Solution:
 
         answer = 0
         odd_vals = []
-        max_odd_val = 0
 
         for key, val in counter.items():
             
@@ -26,4 +25,29 @@ class Solution:
         # if odd_vals == [] : return answer
 
         return answer + sum(odd_vals) - len(odd_vals) + int(odd_vals != [])
+                
+
+    def longestPalindrome(self, s: str) -> int:
+        
+        counter = Counter(s)
+
+        # Construct palindrome with largest odd letters in the middle and 
+        # even letters on each side. 
+
+        # For each letter with odd counts, use all but 1 of that letter
+        # to make it an even count and add to palindrome length
+
+        answer = 0
+        odd_counts = 0
+
+        for key, val in counter.items():
+            
+            if val % 2 == 1 :
+                odd_counts += 1
+                answer += val - 1
+
+            else : 
+                answer += val
+        
+        return answer + int(odd_counts > 0)
                 
