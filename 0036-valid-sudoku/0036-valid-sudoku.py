3428c53fc2285a2c83ebcd0dbae6899c41730720
Suryansh D Kumar <email.suryansh@gmail.com>
2023-12-25 13:04:43 -0500

    Time: 99 ms (69.44%), Space: 17.3 MB (6.45%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -2,7 +2,7 @@ from collections import Counter
 
 class Solution:
 
-    def isValidRow(self, row: List[str]) -> bool:
+    def isValid(self, row: List[str]) -> bool:
 
         counter = Counter(row)
         
@@ -18,18 +18,18 @@ class Solution:
 
         square = [board[i][j] for i in range(row_index, row_index + 3) for j in range(column_index, column_index + 3)]
 
-        return self.isValidRow(square) 
+        return self.isValid(square) 
 
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         
         for row in board:
-            if not self.isValidRow(row): return False
+            if not self.isValid(row): return False
 
         for i in range(len(board)):
 
             column = [board[j][i] for j in range(len(board))]
 
-            if not self.isValidRow(column) : return False
+            if not self.isValid(column) : return False
 
         for i in range(0, len(board), 3):
 
