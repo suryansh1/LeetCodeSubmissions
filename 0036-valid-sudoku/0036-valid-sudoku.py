d696819d8a9ba3805ef5bf89a7f28b802e56aff0
Suryansh D Kumar <email.suryansh@gmail.com>
2023-12-25 13:02:27 -0500

    Time: 89 ms (96.81%), Space: 17.5 MB (6.45%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -0,0 +1,41 @@
+from collections import Counter
+
+class Solution:
+
+    def isValidRow(self, row: List[str]) -> bool:
+
+        counter = Counter(row)
+        
+        for key, val in counter.items():
+
+            if val > 1:
+                if key != '.':
+                    return False
+
+        return True
+
+    def isValidSquare(self, board: List[List[str]], row_index:int, column_index:int) -> bool:
+
+        square = [board[i][j] for i in range(row_index, row_index + 3) for j in range(column_index, column_index + 3)]
+
+        return self.isValidRow(square) 
+
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        
+        for row in board:
+            if not self.isValidRow(row): return False
+
+        for i in range(len(board)):
+
+            column = [board[j][i] for j in range(len(board))]
+
+            if not self.isValidRow(column) : return False
+
+        for i in range(0, len(board), 3):
+
+            for j in range(0, len(board[0]), 3):
+
+                if not self.isValidSquare(board, i, j) : return False
+
+        return True
+
