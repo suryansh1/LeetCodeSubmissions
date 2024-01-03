06eeb0cdcdf3838852ae2f848a7355eef411ab16
Suryansh D Kumar <email.suryansh@gmail.com>
2024-01-03 06:07:43 -0500

    Time: 2640 ms (17.91%), Space: 21.3 MB (28.3%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,10 +1,8 @@
 class Solution:
 
-    def threeSum2pointers(self, nums: List[int]) -> List[List[int]]:
+    def threeSum2pointersDict(self, nums: List[int]) -> List[List[int]]:
 
         size = len(nums)
-
-        answerSet = set()
 
         answer = {}
 
@@ -27,19 +25,45 @@ class Solution:
                     
                     k = myDict[target - nums[j]]
 
-                    # answerSet.add(tuple(sorted([nums[i], nums[j], nums[k]])))
-
                     key = tuple(sorted([nums[i], nums[j], nums[k]]))
 
                     if key not in answer :
 
                         answer[key] = [nums[i], nums[j], nums[k]]
-
    
-        return answer.values()
+        return answer.keys()
 
+    def threeSum2pointersSet(self, nums: List[int]) -> List[List[int]]:
 
+        size = len(nums)
+
+        answerSet = set()
+
+        for i, num in enumerate(nums):
+
+            target = 0 - num
+
+            myDict = {}
+
+            # We need to search for indices which sum to target in nums[i:]
+            # find j,k in nums[i:] such that nums[j] + nums[k] = target
+
+            for j in range(i+1, size) :
+
+                if target - nums[j] not in myDict :
+
+                   myDict[nums[j]] = j
+
+                elif myDict[target - nums[j]] != j: 
+                    
+                    k = myDict[target - nums[j]]
+
+                    answerSet.add(tuple(sorted([nums[i], nums[j], nums[k]])))
+
+        return answerSet
 
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         
-        return self.threeSum2pointers(nums)
+        return self.threeSum2pointersSet(nums)
+
+        return self.threeSum2pointersDict(nums)
