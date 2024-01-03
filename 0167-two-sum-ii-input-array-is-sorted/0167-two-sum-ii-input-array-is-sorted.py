0b82a640a7bc32ddcabf9a8c3a920e5b4a7946bb
Suryansh D Kumar <email.suryansh@gmail.com>
2024-01-03 04:54:32 -0500

    Time: 103 ms (96.04%), Space: 18.8 MB (6.78%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,30 +1,37 @@
 class Solution:
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        
-        # left, right = 0, len(numbers) - 1
 
-        # while (left <= right):
+    def twoSum2pointers(self, numbers: List[int], target: int) -> List[int]:
 
-        #     if numbers[left] + numbers[right] == target :
-        #         return [left+1, right+1]
+        left, right = 0, len(numbers) - 1
 
-        #     if target < numbers[left] + numbers[right] : 
-        #         right -= 1
+        while (left <= right):
 
-        #     # elif target > numbers[left] :
-        #     else : 
-        #         left += 1
+            if numbers[left] + numbers[right] == target :
+                return [left+1, right+1]
 
-        # return [0,0]
+            if target < numbers[left] + numbers[right] : 
+                right -= 1
+
+            # elif target > numbers[left] :
+            else : 
+                left += 1
+
+        return [0,0]
+
+    def twoSumDict(self, numbers: List[int], target: int) -> List[int]:
 
         hashtable = {}
         
         for i,val in enumerate(numbers,start=1):
                       
             if (target - val) in hashtable :
-                
-                # if hashtable[target-val] != i:
-                
+                                
                 return hashtable[target-val], i
             
             hashtable[val] = i
+
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        
+        return self.twoSum2pointers(numbers, target)
+ 
+        
