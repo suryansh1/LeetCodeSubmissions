a83051f6c73150d9936b0c5b3e6f5df26156226b
Suryansh D Kumar <email.suryansh@gmail.com>
2024-01-11 00:38:08 -0500

    Time: 131 ms (94.27%), Space: 18.9 MB (13.57%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -4,6 +4,27 @@ class Solution:
 
     def majorityElement(self, nums: List[int]) -> int:
         
-        counter = Counter(nums)
+        # counter = Counter(nums)
 
-        return counter.most_common(1)[0][0]
+        # return counter.most_common(1)[0][0]
+
+        count = 0
+
+        majority_element = nums[0]
+
+        for i, num in enumerate(nums):
+
+            if num == majority_element:
+
+                count +=1
+
+            else :
+
+                count -=1
+
+                if count == 0 : 
+                    majority_element = num
+                    count = 1
+
+        return majority_element
+    
