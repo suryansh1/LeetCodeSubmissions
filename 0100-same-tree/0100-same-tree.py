3de4717ae9f996dcc974afb52b8b4fbec24d5095
Suryansh D Kumar <email.suryansh@gmail.com>
2024-01-12 23:36:32 -0500

    Time: 34 ms (81.77%), Space: 17.3 MB (18.74%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -5,18 +5,62 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        
+
+    def isSameTreeBFS(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+
         if p is None and q is None : return True
 
         if (p is None and q is not None) or (p is not None and q is None):
 
             return False
 
-        if p.val == q.val :
+        queue = deque()
 
-            if p.left is None and q.left is None and p.right is None and q.right is None : return True
-            
-            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        queue.append(p)
 
-        return False
+        queue.append(q)
+        
+        while (queue):
+
+            pNode = queue.popleft()
+
+            qNode = queue.popleft()
+
+            if (pNode is None and qNode is not None) or (pNode is not None and qNode is None): return False
+
+            if pNode is None and qNode is None : 
+                continue
+
+            if pNode.val == qNode.val : 
+
+                queue.append(pNode.left)
+
+                queue.append(qNode.left)
+
+                queue.append(pNode.right)
+
+                queue.append(qNode.right)
+
+            else : return False
+
+        return True
+
+
+
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        
+        return self.isSameTreeBFS(p,q)
+
+        # if p is None and q is None : return True
+
+        # if (p is None and q is not None) or (p is not None and q is None):
+
+        #     return False
+
+        # if p.val == q.val :
+
+        #     if p.left is None and q.left is None and p.right is None and q.right is None : return True
+
+        #     return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+        # return False
