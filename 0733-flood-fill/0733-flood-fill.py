e039bc52b240788e2013a6249a1367433a93f3bf
Suryansh D Kumar <email.suryansh@gmail.com>
2024-01-15 15:17:00 -0500

    Time: 69 ms (76.46%), Space: 17.4 MB (33.15%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,50 +1,59 @@
 class Solution:
 
-
-
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+
+        queue = deque()
+
+        queue.append((sr,sc))
+
+        visited = set()
+
+        m = len(image)
+
+        n = len(image[0])
+
+        while queue :
+
+            cur = queue.popleft()
+
+            visited.add((cur[0], cur[1]))
+
+
+            # cur[0] + 1, cur[1]
+            # cur[0] - 1, cur[1]
+
+            # cur[0], cur[1] + 1
+            # cur[0], cur[1] - 1
+
+            if cur[0] + 1 < m and (cur[0]+1, cur[1]) not in visited:
+            
+                if image[cur[0]+1][cur[1]] == image[cur[0]][cur[1]] :
+
+                    queue.append((cur[0]+1, cur[1]))
+
+            if cur[0] - 1 >= 0 and (cur[0]-1, cur[1]) not in visited:
+            
+                if image[cur[0]-1][cur[1]] == image[cur[0]][cur[1]] :
+
+                    queue.append((cur[0]-1, cur[1]))
+            
+            if cur[1] + 1 < n and (cur[0], cur[1]+1) not in visited:
+            
+                if image[cur[0]][cur[1] + 1] == image[cur[0]][cur[1]] :
+
+                    queue.append((cur[0], cur[1] + 1))
+            
+            if cur[1] - 1 >= 0 and (cur[0], cur[1] - 1) not in visited:
+            
+                if image[cur[0]][cur[1] - 1] == image[cur[0]][cur[1]] :
+
+                    queue.append((cur[0], cur[1] - 1))
+
+            image[cur[0]][cur[1]] = color
+
         
-        row_count = len(image)
-        column_count = len(image[0])
-
-        q = []
-        q.append([sr, sc])
-        visited = []
-
-        while q :
-            
-            # print(q)
-
-            sr = q[0][0]
-            sc = q[0][1]
-
-            # Check 4 directional and add to queue
-
-            # print (sr, row_count, "\t", sc, column_count, "\t", q)
-
-            if (sr - 1) >= 0 and (image[sr-1][sc] == image[sr][sc]):
-
-                if [sr-1, sc] not in visited : q.append([sr-1, sc])
-
-            if (sc - 1) >= 0 and (image[sr][sc-1] == image[sr][sc]):
-
-                if [sr, sc-1] not in visited : q.append([sr, sc - 1])
-
-            if (sr + 1) < row_count and (image[sr+1][sc] == image[sr][sc]):
-
-                if [sr+1, sc] not in visited : q.append([sr+1, sc])
-            
-            if (sc + 1) < column_count and (image[sr][sc+1] == image[sr][sc]):
-
-                if [sr, sc+1] not in visited : q.append([sr, sc + 1])
-
-            # Flood fill current
-
-            image[sr][sc] = color
-            
-            q.pop(0)
-
-            visited.append([sr,sc])
-
         return image
+
+        
+        
 
