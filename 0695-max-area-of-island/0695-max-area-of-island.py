162c85f298010faaeacba6e90bb975fad2f9bca7
Suryansh D Kumar <email.suryansh@gmail.com>
2024-01-20 00:20:47 -0500

    Time: 115 ms (73.47%), Space: 16.9 MB (90.95%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -4,10 +4,66 @@ class Solution:
 
         return 0 <=i < m and 0 <= j < n
 
+    def maxAreaOfIslandBFSvisitedSet(self, grid: List[List[int]]) -> int:
+
+        island_count = 0
+        max_area = 0
+
+        m = len(grid)
+
+        n = len(grid[0])
+
+        area = 0
+
+        for i in range(m) :
+
+            for j in range(n) : 
+
+                if grid[i][j] == 1 :
+
+                    # Island is found
+
+                    island_count += 1
+
+                    area = 0
+
+                    queue = deque()
+
+                    visited = set()
+
+                    queue.append((i,j))
+
+                    grid[i][j] = 0
+
+                    visited.add((i,j))
+
+                    directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+
+                    while queue : 
+
+                        i, j = queue.popleft()
+
+                        for dx, dy in directions : 
+
+                            x, y = i + dx, j + dy
+
+                            if self.valid(x,y, m, n) and  (x,y) not in visited and grid[x][y] == 1 :
+
+                                queue.append((x, y))
+
+                                visited.add((x,y))
+
+                        grid[i][j] = 0
+
+                        area += 1
+
+                max_area = max(area, max_area)                         
+                                        
+        return max_area 
 
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
 
-        
+        return self.maxAreaOfIslandBFSvisitedSet(grid)
 
         island_count = 0
         max_area = 0
