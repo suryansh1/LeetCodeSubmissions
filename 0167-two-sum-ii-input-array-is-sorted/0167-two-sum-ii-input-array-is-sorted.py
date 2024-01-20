0aad54f008a23d6375d2de4b054bf61b5ef58693
Suryansh D Kumar <email.suryansh@gmail.com>
2024-01-19 23:56:55 -0500

    Time: 103 ms (89.33%), Space: 17.8 MB (55.49%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,5 +1,36 @@
 class Solution:
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+
+    def twoSum2pointers(self, numbers: List[int], target: int) -> List[int]:
+
+        left, right = 0, len(numbers) - 1
+
+        while (left <= right):
+
+            if numbers[left] + numbers[right] == target :
+                return [left+1, right+1]
+
+            if target < numbers[left] + numbers[right] : 
+                right -= 1
+
+            # elif target > numbers[left] :
+            else : 
+                left += 1
+
+        return [0,0]
+
+    def twoSumDict(self, numbers: List[int], target: int) -> List[int]:
+
+        hashtable = {}
+        
+        for i,val in enumerate(numbers,start=1):
+                      
+            if (target - val) in hashtable :
+                                
+                return hashtable[target-val], i
+            
+            hashtable[val] = i
+
+    def twoSumBinarySearch(self, numbers: List[int], target: int) -> List[int]:
 
         size = len(numbers)
 
@@ -9,8 +40,13 @@ class Solution:
 
             index2 = bisect.bisect_left(numbers[i+1:], x)
 
-            # print(i, num, index2, index2 + i + 1)
-
             if index2 + i + 1 == size : index2 -= 1
-            
+
             if numbers[index2+i+1] == x : return i+1, i+index2+2
+
+
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+
+            # return self.twoSum2pointers(numbers, target)
+            # return self.twoSumBinarySearch(numbers, target)
+            return self.twoSumDict(numbers, target)
