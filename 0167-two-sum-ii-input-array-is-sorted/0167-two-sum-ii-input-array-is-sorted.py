1486e05545fa2bb382491805524ed6b998de1de5
Suryansh D Kumar <email.suryansh@gmail.com>
2024-01-19 23:53:00 -0500

    Time: 808 ms (5.06%), Space: 17.5 MB (56.14%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,37 +1,16 @@
 class Solution:
-
-    def twoSum2pointers(self, numbers: List[int], target: int) -> List[int]:
-
-        left, right = 0, len(numbers) - 1
-
-        while (left <= right):
-
-            if numbers[left] + numbers[right] == target :
-                return [left+1, right+1]
-
-            if target < numbers[left] + numbers[right] : 
-                right -= 1
-
-            # elif target > numbers[left] :
-            else : 
-                left += 1
-
-        return [0,0]
-
-    def twoSumDict(self, numbers: List[int], target: int) -> List[int]:
-
-        hashtable = {}
-        
-        for i,val in enumerate(numbers,start=1):
-                      
-            if (target - val) in hashtable :
-                                
-                return hashtable[target-val], i
-            
-            hashtable[val] = i
-
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        
-        return self.twoSum2pointers(numbers, target)
- 
-        
+
+        size = len(numbers)
+
+        for i, num in enumerate(numbers):
+
+            x = target - num
+
+            index2 = bisect.bisect_left(numbers[i+1:], x)
+
+            # print(i, num, index2, index2 + i + 1)
+
+            if index2 + i + 1 == size : index2 -= 1
+            
+            if numbers[index2+i+1] == x : return i+1, i+index2+2
