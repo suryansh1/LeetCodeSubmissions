7117bdc10f5c6a679ef016e1df9f5f3d4c782072
Suryansh D Kumar <email.suryansh@gmail.com>
2024-01-31 11:28:10 -0500

    Time: 691 ms (95.69%), Space: 27.3 MB (83.56%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,23 +1,43 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         
-        profit = 0
-        
-        # Brute force approach
-        
-        # For each index i
-        # Calculate profit from n-i future days and store max 
-        
-        lowest_buy_price = sys.maxsize
-        
-        
-        for i, buy_price in enumerate(prices) : 
-            
-            if buy_price < lowest_buy_price:  lowest_buy_price = buy_price
-            
-            if prices[i] - lowest_buy_price > profit : 
-                
-                profit = prices[i] - lowest_buy_price
-                    
-                    
-        return profit
+        # profit = difference between min_price and future max price
+
+
+        # Scan through the list
+
+        # A candidate day price can either be
+
+        # 1. Lowest price ever seen
+
+            #   In this case, min_price = cur_price
+
+        # Price which increases profit, cur_price - min_price > profit
+
+        if len(prices) == 0 : return 0
+
+        min_price = prices[0]
+        max_profit = 0
+
+        for cur_price in prices:
+
+            if cur_price < min_price : 
+
+                min_price = cur_price
+
+            elif cur_price - min_price > max_profit :
+
+                max_profit = cur_price - min_price
+
+
+        return max_profit
+
+        #   cur_price, min_price, max_profit
+        #   7,7,0
+        #   1,1,0
+        #   5,1,4
+        #   3,1,4
+        #   6,1,5
+        #   4,1,5
+
+
