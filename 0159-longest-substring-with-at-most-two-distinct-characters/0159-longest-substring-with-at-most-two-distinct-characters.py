c6c7181dfc25dd71efb931417fe3dff3c699398e
Suryansh D Kumar <email.suryansh@gmail.com>
2024-01-31 23:18:32 -0500

    Time: 383 ms (21.48%), Space: 17.1 MB (91.53%) - LeetHub

---
+++ 0159-longest-substring-with-at-most-two-distinct-characters/0159-longest-substring-with-at-most-two-distinct-characters.py
@@ -0,0 +1,46 @@
+class Solution:
+    def lengthOfLongestSubstringTwoDistinct(self, s: str) -> int:
+        
+        l = 0
+
+        max_length = 0
+
+        # window = set()
+        counter = Counter()
+
+        for r in range(len(s)):
+
+            if s[r] in counter :
+
+                counter[s[r]] += 1
+
+                max_length = max(max_length, r - l +1)
+
+            elif len(counter) < 2 :
+
+                counter[s[r]] = 1
+                max_length = max(max_length, r - l +1)
+
+            else : 
+
+                while s[l] in counter :
+
+                    counter[s[l]] -= 1
+
+                    if counter[s[l]] == 0 :
+
+                        del(counter[s[l]])
+
+                        l += 1
+                        break
+
+                    l += 1
+
+                counter[s[r]] = 1
+       
+        return max_length
+
+           
+                
+
+
