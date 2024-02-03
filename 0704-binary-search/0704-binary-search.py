83efb148d3f4190037f730021b3268d556286007
Suryansh D Kumar <email.suryansh@gmail.com>
2024-02-03 13:52:42 -0500

    Time: 190 ms (82.08%), Space: 18 MB (74.13%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,22 +1,10 @@
 class Solution:
-
-
     def search(self, nums: List[int], target: int) -> int:
         
-        # insert_index = bisect.bisect_left(nums, target)
-
-        # return -1 if (insert_index == len(nums) or nums[insert_index] != target) else insert_index
-
+        index = bisect.bisect_left(nums, target)        
         
-        low, high = 0, len(nums) - 1
+        if index == len(nums) : return -1
 
-        while low <= high :
-            mid = (low + high)//2
-
-            if target == nums[mid] : return mid
-
-            elif target < nums[mid] : high = mid - 1
-
-            else : low = mid + 1
+        if nums[index] == target : return index
 
         return -1
