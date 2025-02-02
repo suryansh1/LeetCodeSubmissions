da66325e7ec9e9bbc1f18ae5f853774175f211da
Suryansh D Kumar <email.suryansh@gmail.com>
2025-02-02 15:25:29 -0500

    Time: 1847 ms (11.75%), Space: 18.5 MB (62.57%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,54 +1,15 @@
 class Solution:
-
-    def twoSumBruteForce(self, nums: List[int], target: int) -> List[int]:
-
-        for i, num1 in enumerate(nums):
-
-            for j, num2 in enumerate(nums[i+1:]):
-
-                if num1 + num2 == target: return (i, i+j+1)
-
-    def twoSumHashTable(self, nums: List[int], target: int) -> List[int]:
-
-        myDict = {}
-
-        for i, num in enumerate(nums) :
-            myDict[num] = i
-
-        for i, num in enumerate(nums) :
-
-            if (target - num) in myDict:
-
-                if myDict[target - num] != i : 
-                    
-                    return i, myDict[target - num]
-    
-    def twoSumSinglePassHashTable(self, nums: List[int], target: int) -> List[int]:
-
-        myDict = {}
-
-        for i, num in enumerate(nums):
-
-            if (target - num) in myDict :
-
-                return i, myDict[target - num]
-
-            else : myDict[num] = i
-
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        for i in range(len(nums)):
+
+            for j in range(i+1, len(nums)):
+
+                if nums[i] + nums[j] == target :
+
+                    return i,j
         
-        # return self.twoSumBruteForce(nums, target)
 
-        # return self.twoSumHashTable(nums, target)
 
-        # return self.twoSumSinglePassHashTable(nums, target)
 
-        myDict = {}
 
-        for i,num in enumerate(nums) :
-
-            if target - num in myDict :
-
-                return i, myDict[target - num]
-
-            else : myDict[num] = i
